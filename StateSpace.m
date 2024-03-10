304edacabb5b2ca26f1ba330da410a1497e536a8
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2024-03-10 22:59:23 +0100

    Comments

--- StateSpace.m
+++ StateSpace.m
@@ -1,7 +1,8 @@
 %% Entering State-Space
 % https://www.youtube.com/watch?v=TKBm1GzxUO8&t=1208s
-% This video illustrates how to build a statespace model from a drone
-% So far
+% This video illustrates how to build a statespace model from/for a drone.
+% The naming references can be found in the respective folder in the
+% repository
 
 
 % Parameters
