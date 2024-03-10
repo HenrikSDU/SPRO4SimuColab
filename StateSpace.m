57711b00e6c7a7be4eb8b54387dbca2c52bc33d0
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2024-03-10 23:06:44 +0100

    Comments

--- StateSpace.m
+++ StateSpace.m
@@ -1,8 +1,8 @@
 %% Entering State-Space
 % https://www.youtube.com/watch?v=TKBm1GzxUO8&t=1208s
 % This video illustrates how to build a statespace model from/for a drone.
-% The naming references can be found in the respective folder in the
-% repository
+% The naming references can be found on teams under Modeling -> Matlab &
+% Simulink.
 
 
 % Parameters
