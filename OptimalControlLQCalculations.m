dde6813849b9c1e10c2108150f59256552143b09
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2024-04-11 13:33:43 +0200

    LQI Extended Statespace

--- OptimalControlLQCalculations.m
+++ OptimalControlLQCalculations.m
@@ -3,7 +3,7 @@
 %% Controlability & Reachability
 
 C_r = ctrb(A,B);
-rank(C_r)
+rank(C_r);
 %% LQR Section
 
 
@@ -13,3 +13,13 @@ rank(C_r)
 %% LQI Section
 
 % Define Extended State Space
+A_e = [A zeros(12,6);
+       C zeros(6,6);];
+B_e = [B;
+      -D];
+
+C_e = [C zeros(6,6)];
+
+D_e = D;
+
+C_r_lqi = ctr
