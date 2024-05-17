3ae29f91cf7389708bb7ed0184d3ec5579fcf96b
dario-szel <dario.szel01@gmail.com>
2024-05-17 12:43:15 +0200

    trying to make it work

--- BBB_tets/BBB_calibration_measurements.m
+++ BBB_tets/BBB_calibration_measurements.m
@@ -1,3 +1,13 @@
 %simout_bbb=sim("C:\Users\Dario\Desktop\semester 4\Project\matlab & simulink\Drone simulink\SPRO4SimuColab\BBB_tets\BBB_calibration.slx", "StopTime", "10");
+bbblue = beagleboneblue;
+%stop the model from running on BBB
+%stopModel(bbblue,'BBB_calibration')
 
-ScopeData.signals.values
+%check if model is running, 1 if it is, 0 if it is not
+%isModelRunning(bbblue,'BBB_calibration')
+
+%see contents of a log file
+system(bbblue,'cat BBB_calibration.log')
+
+%getFiule saves a file to a current matlab directory
+%getFile(bbblue,'BBB_calibration.log')
