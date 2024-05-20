37b6310c8d8cc9809f81f5541990072e3ca666a0
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2024-05-20 19:10:35 +0200

    Kalman Tuning

--- reduced_state_space.m
+++ reduced_state_space.m
@@ -43,7 +43,7 @@ end
 
 x0_r = [0;0;0;0;0;0;0;0;];%initial condition
 %%
-Max_z_r = 0.4; % Max distance allowed on z _axis (m)
+Max_z_r = 0.05; % Max distance allowed on z _axis (m)
 Max_r_r = 0.0002*(pi/180); % Max roll allowed (°)
 Max_p_r = 0.0002 *(pi/180); % Max pitch allowed (°)
 Max_ya_r = 2 *(pi/180); % Max yaw allowed (°)
