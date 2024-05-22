bff9b27af281619d9f4fd46537b5fb564f53ebd1
dario-szel <dario.szel01@gmail.com>
2024-05-22 09:18:49 +0200

    controller tuning

--- discrete_control.m
+++ discrete_control.m
@@ -4,9 +4,12 @@ Ts = 0.001;
 
 discrete_sys_reduced = c2d(sys_reduced,Ts);
 [A_r_d,B_r_d,C_r_d,D_r_d] = ssdata(discrete_sys_reduced);
+Ad = discrete_sys_reduced.B
 
 
 K_D_LQR_reduced = lqrd(A_r, B_r, Q_reduced, R_reduced, Ts);
 K_DD_LQR_reduced_ = lqrd(A_r, B_r, Q_reduced, R_reduced, Ts);
 K_LQR_reduced;
+disp(Ad)
+disp(B_r_d)
 
