c95ab9b385dab713d67c3230826cbfaaac4607aa
dario-szel <dario.szel01@gmail.com>
2024-05-23 11:20:45 +0200

    x

---
+++ DroneModel_ert_rtw/DroneModel_private.h
@@ -0,0 +1,33 @@
+/*
+ * DroneModel_private.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "DroneModel".
+ *
+ * Model version              : 3.13
+ * Simulink Coder version : 24.1 (R2024a) 19-Nov-2023
+ * C source code generated on : Mon May 13 16:34:50 2024
+ *
+ * Target selection: ert.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef DroneModel_private_h_
+#define DroneModel_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "DroneModel_types.h"
+#include "rtw_continuous.h"
+#include "rtw_solver.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmSetTFinal
+#define rtmSetTFinal(rtm, val)         ((rtm)->Timing.tFinal = (val))
+#endif
+#endif                                 /* DroneModel_private_h_ */
